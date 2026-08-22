d7c323695c2ad0fff589e20930109d05670782c1
goldy thakur <goldykumar0807@gmail.com>
2026-08-22 12:11:07 +0530

    Optimize bubble sort loop condition

--- 05_sorting/bubble_sort_algorithm.cpp
+++ 05_sorting/bubble_sort_algorithm.cpp
@@ -15,13 +15,13 @@ int main(){
     }
     for(int i=0;i<n-1;i++){
         bool flag=true;
-        for(int j=0;j<n-1;j++){
+        for(int j=0;j<n-1-i;j++){
             if(v[j]>v[j+1]){
                 swap(v[j],v[j+1]);
                 flag=false;
             }
-            if(flag) break;
         }
+        if(flag==true) break;
     }
     for(int i=0;i<n;i++){
         cout<<v[i]<<" ";
