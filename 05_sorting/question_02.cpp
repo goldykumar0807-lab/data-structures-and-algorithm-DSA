892f7e3e473d4169300afc49503cd79c91a74906
goldy thakur <goldykumar0807@gmail.com>
2026-08-22 12:47:53 +0530

    Add program to move zeroes to the end of array
    Implement a program to push zeroes to the end of an array while maintaining the relative order of other elements.

---
+++ 05_sorting/question_02.cpp
@@ -0,0 +1,29 @@
+//push zeroes to end while maintaining the relative order of other elements.
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter size of array : ";
+    cin>>n;
+    vector<int> v;
+    cout<<"enter elements of array : ";
+    for(int i=0;i<n;i++){
+        int x;
+        cin>>x;
+        v.push_back(x);
+    }
+    for(int i=0;i<n-1;i++){
+        bool flag = true;
+        for(int j=0;j<n-1-i;j++){
+            if(v[j]==0 && v[j+1]!=0){
+                swap(v[j],v[j+1]);
+                flag=false;
+            }
+        }
+        if(flag) break;
+    }
+    for(int i=0;i<n;i++){
+        cout<<v[i]<<" ";
+    }
+}
