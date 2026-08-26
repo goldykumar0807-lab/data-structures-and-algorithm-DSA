8efb7e87d9e7630aa047cfec82a3991b7d07d16e
goldy thakur <goldykumar0807@gmail.com>
2026-08-26 14:51:57 +0530

    Implement program to find first occurrence in array

---
+++ 01_arrays/program_to_find_first_occurrence.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter size of array : ";
+    cin>>n;
+    vector<int> v(n);
+    cout<<"enter elements of array : ";
+    for(int i=0;i<n;i++){
+        cin>>v[i];
+    }
+    sort(v.begin(),v.end());
+    int x;
+    cout<<"enter target : ";
+    cin>>x;
+    int low=0;
+    int high=n-1;
+    int idx=-1;
+    while(low<=high){
+        int mid=low+((high-low)/2);
+        if(v[mid]==x) {
+            idx=mid;
+            break;
+           
+        }
+        else if (v[mid]>x) high=mid-1;
+        else if(v[mid]<x) low=mid+1; 
+    }
+    cout<<"first occurrence of element is at index : "<<idx;
+}
