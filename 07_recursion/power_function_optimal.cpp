e61b669a40282cebf1815e199272bea80d886d98
goldy thakur <goldykumar0807@gmail.com>
2026-09-04 00:51:18 +0530

    Add optimal power function implementation
    Implement optimal power function using recursion.

---
+++ 07_recursion/power_function_optimal.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int pow(int a,int b){
+    if(b==1) return a;
+    if(b%2==0) return pow(a,b/2)*pow(a,b/2);
+    return a*pow(a,b-1);
+}
+int main(){
+    int a;
+    cout<<"enter value of base : ";
+    cin>>a;
+    int b;
+    cout<<"enter value of exponent : ";
+    cin>>b;
+    cout<<pow(a,b);
+}
