6003c2949ca2fefc6040e58ac35de661f1b19754
goldy thakur <goldykumar0807@gmail.com>
2026-09-04 00:58:14 +0530

    Optimize power function for even exponents
    Optimize power function to handle even exponents more efficiently.

--- 07_recursion/power_function_optimal.cpp
+++ 07_recursion/power_function_optimal.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 using namespace std;
 int pow(int a,int b){
+    if(b==0) return 1;
     if(b==1) return a;
     if(b%2==0) return pow(a,b/2)*pow(a,b/2);
     return a*pow(a,b-1);
