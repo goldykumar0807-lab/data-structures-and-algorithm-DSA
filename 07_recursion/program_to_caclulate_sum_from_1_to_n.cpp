abc9750770c23049a2348acbcb74250cdb2f413b
goldy thakur <goldykumar0807@gmail.com>
2026-09-03 10:28:52 +0530

    Add recursive function to calculate sum from 1 to n

---
+++ 07_recursion/program_to_caclulate_sum_from_1_to_n.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int sum(int i,int n){
+    if(i>n) return 0;
+    return i+sum(i+1,n);
+}
+int main(){
+    int n;
+    cout<<"enter value of n : ";
+    cin>>n;
+    cout<<sum(1,n);
+}
