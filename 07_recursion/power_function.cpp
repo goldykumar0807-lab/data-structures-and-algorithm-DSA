d6840511ffb3ec9d26a65847da1a8c76cec74c9e
goldy thakur <goldykumar0807@gmail.com>
2026-09-03 10:49:05 +0530

    Add check for undefined case of 0 raised to 0
    Handle case for 0^0 as not defined in power function.

--- 07_recursion/power_function.cpp
+++ 07_recursion/power_function.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 int exponent(int a,int b){
     if(b==0) return 1;
+    if(a==0) return 0;
     return a*exponent(a,b-1);
 }
 int main(){
@@ -11,5 +12,6 @@ int main(){
     int b;
     cout<<"enter exponent : ";
     cin>>b;
-    cout<<exponent(a,b);
+    if(b==0 && a==0 ) cout<<"not defined. ";
+    else cout<<exponent(a,b);
 }
