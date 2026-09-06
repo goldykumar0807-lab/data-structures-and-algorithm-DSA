ba00472bca134e60f09bf5da7b0a025885c30b53
goldy thakur <goldykumar0807@gmail.com>
2026-09-07 02:02:59 +0530

    Implement recursive palindrome checker

---
+++ 07_recursion/palindrome_using_recursion.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<string>
+using namespace std;
+string palindrome(string s, int i,int j){
+    if(i>j) return "yes";
+    if(s[i]!=s[s.size()-1-i]) return"no";
+    return palindrome(s,i+1,j-1);
+}
+int main(){
+    string s;
+    cout<<"enter a string : ";
+    cin>>s;
+    cout<<palindrome(s,0,s.size()-1);
+}
